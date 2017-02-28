81c7f388f12e49465754cb8981e3c21fcf1b0a85
michal <mic132@gazeta.pl>
2017-02-28 12:53:13 +0100

    Drawing quotes moved to service.
    (cherry picked from commit 4f748ec)

--- app.js
+++ app.js
@@ -4,18 +4,31 @@
 (function () {
 
     var Component = ng.core.Component;
-    var NgModule =  ng.core.NgModule;
+    var NgModule = ng.core.NgModule;
     var BrowserModule = ng.platformBrowser.BrowserModule;
     var platformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic();
+    var Class = ng.core.Class;
+
+    var QuoteService = Class({
+        constructor: function () {
+            this.quotes = quotes2;
+        },
+
+        getRandomQuote: function () {
+            var randomIndex = Math.floor(Math.random() * this.quotes.length);
+            return this.quotes[randomIndex];
+        }
+    })
+
 
     var SecondComponent = Component({
         selector: 'second',
         template: '<p><em>{{quote.line}}</em> - {{quote.author}}</p>'
     })
         .Class({
-            constructor : function () {
-                var randomIndex = Math.floor(Math.random()*quotes.length);
-                this.quote = quotes[randomIndex];
+            constructor: function () {
+                var quoteService = new QuoteService();
+                this.quote = quoteService.getRandomQuote();
             }
         })
 
@@ -32,34 +45,35 @@
     var AppModule = NgModule({
         imports: [BrowserModule],
         declarations: [AppComponent, SecondComponent],
+        providers: [QuoteService],
         bootstrap: [AppComponent]
     }).Class({
-            constructor: function(){}
-        })
+        constructor: function () {}
+    })
 
     platformBrowserDynamic.bootstrapModule(AppModule);
 
-    var quotes = [
-            {
-                'line':"Życie jest jak pudełko czekoladek - nigdy nie wiesz, co ci się trafi.",
-                'author':"Forrest Gump"
-            },
-            {
-                'line':" świat się zmienia, słońce zachodzi a wódka się kończy.",
-                'author':"Andrzej Sapkowski"
-            },
-            {
-                'line':"Prawo pędzenia bimbru jest prawem człowieka.",
-                'author':"Kroniki Jakuba Wędrowycza"
-            },
-            {
-                'line':" Rosjanie maja trzy waluty: ruble, dolary i wódkę.",
-                'author':"Władimir Żelwis "
-            },
-            {
-                'line':"Panie Boże, lubiłem dżem truskawkowy/ I ciemną słodycz kobiecego ciała./ Jak też wódkę mrożoną, śledzie w oliwie.",
-                'author':"Czesław Miłosz"
-            }
-        ]
+    var quotes2 = [
+        {
+            'line': "Życie jest jak pudełko czekoladek - nigdy nie wiesz, co ci się trafi.",
+            'author': "Forrest Gump"
+        },
+        {
+            'line': " świat się zmienia, słońce zachodzi a wódka się kończy.",
+            'author': "Andrzej Sapkowski"
+        },
+        {
+            'line': "Prawo pędzenia bimbru jest prawem człowieka.",
+            'author': "Kroniki Jakuba Wędrowycza"
+        },
+        {
+            'line': " Rosjanie maja trzy waluty: ruble, dolary i wódkę.",
+            'author': "Władimir Żelwis "
+        },
+        {
+            'line': "Panie Boże, lubiłem dżem truskawkowy/ I ciemną słodycz kobiecego ciała./ Jak też wódkę mrożoną, śledzie w oliwie.",
+            'author': "Czesław Miłosz"
+        }
+    ]
 
 })();
